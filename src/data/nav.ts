ace7747d8ae5c93d315396f76b48863cc7c3a4dc
vos6434 <kacper11122003@wp.pl>
2022-06-30 14:13:32 +0100

    Update nav.ts

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,7 +1,7 @@
 export const nav = [
   {
     title: 'News',
-    slug: 'https://gentle-dodol-d2ed0d.netlify.app',
+    slug: 'https://galacticraft-test.netlify.app',
   },
   {
     title: 'Discord',
