8fd84f9531bacec57df3b0f329eb02412bfc28f7
vos6434 <kacper11122003@wp.pl>
2022-06-30 12:41:25 +0100

    Add files via upload

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,10 +1,14 @@
 export const nav = [
   {
     title: 'News',
-    slug: 'https://62bd890dde6f9a0008828da5--gentle-dodol-d2ed0d.netlify.app/',
+    slug: 'https://gentle-dodol-d2ed0d.netlify.app',
+  },
+  {
+    title: 'Discord',
+    slug: 'https://discord.gg/n3QqhMYyFK',
   },
   {
     title: 'Forum',
-    slug: 'https://forum.micdoodle8.com/index.php',
+    slug: 'https://forum.micdoodle8.com',
   },
 ]
